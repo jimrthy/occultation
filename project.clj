db7977b3b13c1a645e8be7fd915e2dc69b04c010
Jon Distad <jon.distad@gmail.com>
2013-05-09 19:06:10 -0400

    Update deps for Java 7

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
                  [cantor "0.3.0"]
                  [org.clojure/clojure "1.2.0"] 
                  [org.clojure/clojure-contrib "1.2.0"]
-				 [org.clojars.charles-stain/lwjgl "3.0"]
-				 [org.lwjgl/lwjgl-util "2.7.1"]
-				 [org.clojars.charles-stain/jme3-lwjgl-natives "3.0"]]
+                 [org.lwjgl.lwjgl/lwjgl_util "2.9.0"]
+                 [org.lwjgl.lwjgl/lwjgl "2.9.0"]]
   :java-source-paths ["java"]
   :dev-dependencies [[swank-clojure "1.3.0"]])

54612886187eaccb86a80798348444f8504f6c8a
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-05-20 13:32:57 -0400

    Correcting java-source-paths for modern lein.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject kephale/penumbra "0.6.3-SNAPSHOT"
-  :description "An idiomatic wrapper for OpenGL. Clojure 1.3 version"
+  :description "An idiomatic wrapper for OpenGL."
   :dependencies [[slick-util "1.0.0"]
                  [kephale/cantor "0.4.0-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"] 
-		 [org.clojure/math.combinatorics "0.0.2"]
-		 [org.clojars.charles-stain/lwjgl "3.0"]
-		 [org.lwjgl/lwjgl-util "2.7.1"]
-		 [org.clojars.charles-stain/jme3-lwjgl-natives "3.0"]]
-;  :source-paths ["src"]
-  :java-source-path ["java"]
+                 [org.clojure/math.combinatorics "0.0.2"]
+                 [org.clojars.charles-stain/lwjgl "3.0"]
+                 [org.lwjgl/lwjgl-util "2.7.1"]
+                 [org.clojars.charles-stain/jme3-lwjgl-natives "3.0"]]
+  :java-source-paths ["java"]
   :checksum-deps false)

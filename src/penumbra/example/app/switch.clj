e33f6965319ec86aafd562a0e57f536cb8e7cff5
James Gatannah <james.gatannah@gmail.com>
2015-01-28 20:17:43 -0600

    Roll switch back to Zach's original

--- src/penumbra/example/app/switch.clj
+++ src/penumbra/example/app/switch.clj
@@ -6,58 +6,21 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-;;;; Seems to be a basic window manager. When you press the <ESC> key, it
-;;;; looks like it should switch back and forth between App1 and App2.
-
-(ns penumbra.example.app.switch
+(ns example.app.switch
   (:use [penumbra opengl text])
-  (:require [penumbra.app :as app]
-            [clojure.pprint :refer [pprint with-pprint-dispatch]]
-            [slingshot.slingshot :refer [try+ throw+]]))
+  (:require [penumbra.app :as app]))
 
-(defn switch [a apps]
-  (println "Switching to " a)
-  (try+
-   (with-pprint-dispatch print
-     ;; FIXME: What is this? There's a reason I've spent so
-     ;; much time trying to take a look at this.
-     ;; As a random thought, give
-     ;; a shot, somewhere higher up the callstack (most likely)
-     (pprint apps)
-     (throw+ "FIXME: Start here"))   
-   (catch Exception ex
-     (println "Pretty-printing apps failed:\n" ex)
-     ;; TODO: Start here with this.
-     (throw (RuntimeException. "Yes. Right here."))
-     (println "Apparently I need to pick a priority for printing an App."))
-   (catch Object ex
-     (println "Pretty-printing apps failed unexpectedly:\n" ex)
-     (throw+)))
-  (try
-    (loop [app (apps a)]
-      (when app
-        (recur (-> app app/start deref :goto apps))))
-    (catch Exception ex
-      (println "Unhandled exception in switch:")
-      (pprint ex))))
+(declare apps)
+
+(defn switch [a]
+  (loop [app (apps a)]
+    (when app
+      (recur (-> app app/start deref :goto apps)))))
 
 ;; Controller
 
 (defn controller-init [state]
-  (println "Initializing controller")
-  (switch :first state))
-
-(comment (defn controller-draw [state]
-           (println "Drawing controller")
-           (translate 0 -0.93 -3)
-           (draw-triangles
-            (color 1 0 0) (vertex 1 0)
-            (color 0 1 0) (vertex -1 0)
-            (color 0 0 1) (vertex 0 1.86))
-           ;; Not that we ever get to here.
-           (when-let [next (:goto state)]
-             (switch next state))
-           (app/repaint!)))
+  (switch :first))
 
 ;; First app
 
@@ -65,23 +28,12 @@
   (assoc state :goto nil))
 
 (defn first-key-press [key state]
-  (println "1: " key)
   (when (= key :escape)
-    ;; Actually stopping the app seems like a mistake.
-    (comment
-      ;; Getting rid of this at least keeps the app from crashing.
-      ;; It also eliminates the switch. I wonder what was planned for this.
-      ;; Not a matter of planned. This was pretty obviously working.
-      )
-    (do
-      (println "Stopping current app")
-      (app/stop!))
-    (println "Ordering change to to second window")
+    (app/stop!)
     (assoc state :goto :second)))
 
 (defn first-display [_ state]
-  (write-to-screen "first app" 0 0)
-  (comment (app/repaint!)))
+  (write-to-screen "first app" 0 0))
 
 ;; Second app
 
@@ -89,23 +41,18 @@
   (assoc state :goto nil))
 
 (defn second-key-press [key state]
-  (println "2: " key)
   (when (= key :escape)
     (app/stop!)
     (assoc state :goto :first)))
 
 (defn second-display [_ state]
-  (comment) (println "Display second")
-  (write-to-screen "second app" 0 0)
-  (comment (app/repaint!)))
+  (write-to-screen "second app" 0 0))
 
 ;;
 
+(def apps {:controller (app/create {:init controller-init} {})
+           :first (app/create {:init first-init, :key-press first-key-press, :display first-display} {})
+           :second (app/create {:init second-init, :key-press second-key-press :display second-display} {})})
+
 (defn start []
-  (println "Begin")
-  (let [first (app/create {:init first-init, :key-press first-key-press, :display first-display} {})
-        second (app/create {:init second-init, :key-press second-key-press :display second-display} {})
-        controller (app/create {:init controller-init} {:first first :second second})]
-    (comment (app/start {:init controller-init :display controller-draw} 
-                        {:first first :second second}))
-    (app/start controller)))
+  (app/start (:controller apps)))

5f4cdeb33ff3e334bf8a86c35e02a36e7f54ae20
James Gatannah <james.gatannah@gmail.com>
2015-02-09 21:14:04 -0600

    Fatal IO Error 11
    This was happening after calling (reset) 2 or 3 times. Couldn't get a
    core dump (apparently that's one of the 'typical' symptoms), and google
    failed me pretty much completely.
    
    There was a java error log that pointed back to glfwTerminate. So took
    that out of the equation. Just call initialize once, then never call
    terminate.
    
    It feels dirty, but seems to be a valid work-around for now

--- src/penumbra/base.clj
+++ src/penumbra/base.clj
@@ -6,7 +6,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Schema
 
-(defrecord BaseSystem [error-callback]
+(defrecord BaseSystem [error-callback initialized]
   component/Lifecycle
   (start
     [this]
@@ -17,11 +17,20 @@
                    (let [s (Callbacks/errorCallbackDescriptionString description)]
                      (error-callback error-code s))))]
         (GLFW/glfwSetErrorCallback cb)))
-    (GLFW/glfwInit)
-    this)
+    (when-not initialized
+      (GLFW/glfwInit))
+    (assoc this :initialized true))
   (stop
     [this]
-    (GLFW/glfwTerminate)
+    ;; I'm getting a SIGSEGV here. It has something to do with a mysterious
+    ;; XIO Fatal IO Error 11.
+    ;; The best hint google's supplied so far is that it might have something
+    ;; to do with multi-threading.
+    ;; Q: What are the odds that this gets called from something besides the
+    ;; main thread?
+    ;; I've tried it in both emacs and reply, but they're both going over
+    ;; nrepl to what I think is the same back-end.
+    (comment (GLFW/glfwTerminate))
     this))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
